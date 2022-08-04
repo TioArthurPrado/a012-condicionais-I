b2df5330b211e0c120b2854ff0010d47e6b38a31
Arthur Prado <arthur.matheus.prado@gmail.com>
2022-08-03 22:56:18 -0300

    barbosa-aula12

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,7 @@
-const num = //seu numero aqui
+const num = prompt('INSIRA UM NUMERO AQUI')
 
-if(){
-    console.log("sua mensagem aqui");
+if(num % 2 === 0){
+    console.log("Seu numero é par");
+}else{
+    console.log('Seu numero é impar')
 }
